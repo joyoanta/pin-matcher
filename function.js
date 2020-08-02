2aa97485c3c846c972a6b2ce228e2877849bf1e6
joyoanta <banik.joyoanta12@gmail.com>
2020-08-03 01:17:17 +0600

    all buttion working

--- function.js
+++ function.js
@@ -1,6 +1,7 @@
 document.getElementById("generate-pin").addEventListener('click', function(){
     let pin = Math.floor(1000 + Math.random() * 9000);
     document.getElementById("display-pin").value = pin; 
+
 })
 
 
@@ -21,12 +22,49 @@ document.getElementById("generate-pin").addEventListener('click', function(){
     keyStroke("nine");
     keyStroke("zero");
 
-
-
 function keyStroke(id){
     document.getElementById(id).addEventListener("click",function(){
     let number =  document.getElementById(id).innerText; 
     let newnumber=  document.getElementById("display-number").value 
+
     document.getElementById("display-number").value = newnumber + number;
     })
 }
+
+// Submit buttion 
+
+document.getElementById("submit").addEventListener('click', function(){
+
+    let pinDigit =  document.getElementById("display-pin").value ; 
+    let inputDigit = document.getElementById("display-number").value ; 
+
+if (pinDigit == inputDigit) {
+
+document.getElementById("display-number").value = "" ;
+document.getElementById("right").style.display = "block"; 
+   
+}
+
+else {
+
+    document.getElementById("display-number").value = "" ;
+    document.getElementById("wrong").style.display = "block";  
+}
+})
+
+// C button 
+
+document.getElementById("clear").addEventListener('click', function(){
+
+    document.getElementById("display-number").value = null; 
+})
+
+// > backspace buttion 
+
+document.getElementById("remove").addEventListener('click', function(){
+
+    var backspace = document.getElementById("display-number")
+    backspace.value =backspace.value.substring(0, backspace.value.length - 1);
+    
+
+})
